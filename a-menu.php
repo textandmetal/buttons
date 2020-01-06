acc52a7573cad2d6f95656aeab7cc4c4424e4a46
textandmetal <textandmetal@gmail.com>
2020-01-06 23:08:12 +0000

    yes

--- a-menu.php
+++ a-menu.php
@@ -1,5 +1,4 @@
 <?php
-define("ROW_PER_PAGE",2);
 // include database connection file
 include("dbconfig.php");
 ?>
@@ -34,36 +33,6 @@ echo "<script>window.location.href='index.php'</script>";
 		$search_keyword = $_POST['search']['keyword'];
 	}
 	$sql = 'SELECT * FROM articles WHERE topic LIKE :keyword  ORDER BY id DESC ';
-	
-	/* Pagination Code starts 
-	$per_page_html = '';
-	$page = 1;
-	$start=0;
-	if(!empty($_POST["page"])) {
-		$page = $_POST["page"];
-		$start=($page-1) * ROW_PER_PAGE;
-	}
-	$limit=" limit " . $start . "," . ROW_PER_PAGE;
-	$pagination_statement = $pdo_conn->prepare($sql);
-	$pagination_statement->bindValue(':keyword', '%' . $search_keyword . '%', PDO::PARAM_STR);
-	$pagination_statement->execute();
-
-	$row_count = $pagination_statement->rowCount();
-	if(!empty($row_count)){
-		$per_page_html .= "<div style='text-align:center;margin:20px 0px;'>";
-		$page_count=ceil($row_count/ROW_PER_PAGE);
-		if($page_count>1) {
-			for($i=1;$i<=$page_count;$i++){
-				if($i==$page){
-					$per_page_html .= '<input type="submit" name="page" value="' . $i . '" class="btn-page current" />';
-				} else {
-					$per_page_html .= '<input type="submit" name="page" value="' . $i . '" class="btn-page" />';
-				}
-			}
-		}
-		$per_page_html .= "</div>";
-	}*/
-	
 	$query = $sql.$limit;
 	$pdo_statement = $pdo_conn->prepare($query);
 	$pdo_statement->bindValue(':keyword', '%' . $search_keyword . '%', PDO::PARAM_STR);
@@ -80,6 +49,3 @@ echo "<script>window.location.href='index.php'</script>";
 		}
 	}
 	?>
-
-<?php echo $per_page_html; ?>
-
