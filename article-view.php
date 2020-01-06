6d372c5365118c4dd13b9db7383d373c11d0f303
textandmetal <textandmetal@gmail.com>
2020-01-06 23:44:44 +0000

    yes

--- article-view.php
+++ article-view.php
@@ -25,6 +25,16 @@ if($query->rowCount() > 0)
 foreach($results as $result)
 {               
 ?>
+		<?php include 'php/head.html'; ?>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<!-- Banner -->
+					<section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+						<div class="content">
+
 <h1><a href="/"><?php echo htmlentities($result->type);?></a></h1>
 
 <?php
@@ -53,3 +63,20 @@ foreach($results as $result)
 
 
 <?php }} ?>
+						</div>
+					<!--	<div class="image">
+							<img src="images/thinkpad.png" alt="" />
+						</div> -->
+					</section>
+
+
+
+	
+				
+			</div>
+
+
+		<?php include 'php/scripts.html'; ?>
+
+	</body>
+</html>
