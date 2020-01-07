f0b9eb47dac1a14036953f597fbbd7f495c1009e
textandmetal <textandmetal@gmail.com>
2020-01-07 02:07:14 +0000

    yes

--- articles/about.php
+++ articles/about.php
@@ -10,12 +10,7 @@
                           
 		<?php include '../php/name.html'; ?>
 							<p>This website is designed to provide an interace to access information for clients, workers, and students.</p>
-							<p>The Built Environment is an exciting discipline to be a be working as
-                            a professional, with the right resources and tools you can play an
-                            integral part of the development and creation of infrastructure. Check out our <a href="../submenus/google.php">Google</a> page and out <a href="../submenus/linux.php">Linux</a> page for technology and building.</p>
-							<p>Download a blank wiki to make your own by clicking the button.</p>
 							<p><a href="sitemap.php" class="button big wide smooth-scroll-middle">Site Map</a></p>
-							<p><a href="../index.php" class="button big wide smooth-scroll-middle" >Home</a></p>
               
 						<p>
 							<a href="../index.php" class="button big wide smooth-scroll-middle" onclick="goBack()">Back</a>
