c170a810f54c350604563021a81a26877be274c8
textandmetal <textandmetal@gmail.com>
2020-01-07 03:08:40 +0000

    yes

--- sitemap.php
+++ sitemap.php
@@ -15,41 +15,21 @@
 							<p>This is a simple map to our website, like the contents of a book this page lists all the articles written in this format.</p>
 
 
-<header><h3>Construction Management</h3></header>
+<header><h3>Vim</h3></header>
 <ul>
 <li><a href="">Introduction to Construction Management</a></li>
-<li><a href="">Communication in Construction Management</a></li>
-<li><a href="">The Law and Construction Management</a></li>
-<li><a href="">Tendering Administration Process for the Construction Industry</a></li>
-<li><a href="">Construction Programming and Methodology</a></li>
-</ul>
-
-<header><h3>Site Processes</h3></header>
-<ul>
-<li><a href="">Foundations</a></li>
-<li><a href="">Subfloor</a></li>
-<li><a href="">Flooring</a></li>
-<li><a href="">Framing</a></li>
-<li><a href="">Hardware</a></li>
-<li><a href="">Roofing</a></li>
-<li><a href="">Preclad</a></li>
-<li><a href="">Cladding</a></li>
-<li><a href="">Windows</a></li>
-<li><a href="">Insulation</a></li>
-<li><a href="">Lining</a></li>
-<li><a href="">Doors</a></li>
-<li><a href="">Trim</a></li>
-</ul>
-
-<header><h3>Site Controls</h3></header>
-<ul>
-<li><a href="">Tidy Site</a></li>
 </ul>
 
 <header><h3>Linux</h3></header>
 <ul>
-<li><a href="">Vim</a></li>
+<li><a href="">Foundations</a></li>
 </ul>
+
+<header><h3>Bash Scripts</h3></header>
+<ul>
+<li><a href="">Tidy Site</a></li>
+</ul>
+
 							<p><a href="../index.php" class="button big wide smooth-scroll-middle" >Home</a></p>
               
 						</div>
