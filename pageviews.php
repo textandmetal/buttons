86afc9602e654be42516cd0a97e885c8f454a86c
textandmetal <textandmetal@gmail.com>
2020-01-07 10:26:17 +0000

    yes

--- pageviews.php
+++ pageviews.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "root", "GreenJeans33Winter1@", "buttons");
 
-$result = mysqli_query($link, "SELECT * FROM "analytics);
+$result = mysqli_query($link, "SELECT * FROM analytics");
 $num_rows = mysqli_num_rows($result);
 
 echo "$num_rows Rows\n";
