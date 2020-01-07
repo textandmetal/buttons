fe7a1f53480611028d3554aba264a4d8684b5327
textandmetal <textandmetal@gmail.com>
2020-01-07 10:43:39 +0000

    yes

--- analytics/aboutviews.php
+++ analytics/aboutviews.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "root", "GreenJeans33Winter1@", "buttons");
 
-$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = 'about.php'");
+$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = '/about.php'");
 $num_rows = mysqli_num_rows($result);
 
 echo "$num_rows About Page Views\n";
