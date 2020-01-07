15d2e21c4e39e8f44e9ba7d1cd6f04f51d34eaab
textandmetal <textandmetal@gmail.com>
2020-01-07 10:36:20 +0000

    yes

--- analytics/individualviews.php
+++ analytics/individualviews.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "root", "GreenJeans33Winter1@", "buttons");
 
-$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = /");
+$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = '/'");
 $num_rows = mysqli_num_rows($result);
 
 echo "$num_rows Home Page Visits\n";
