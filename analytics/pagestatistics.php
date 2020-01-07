6c4d5aa47ca538b7a70ccd21030d153f9f997d4c
textandmetal <textandmetal@gmail.com>
2020-01-07 12:07:16 +0000

    yes

--- analytics/pagestatistics.php
+++ analytics/pagestatistics.php
@@ -29,53 +29,6 @@ body{width:615px;font-family:arial;letter-spacing:1px;line-height:20px;}
 <?php include("articleviews.php"); ?><br>
 
 <hr />
-<?php	
-	$search_keyword = '';
-	if(!empty($_POST['search']['keyword'])) {
-		$search_keyword = $_POST['search']['keyword'];
-	}
-	$sql = 'SELECT * FROM analytics WHERE pageurl LIKE :keyword OR urlquery LIKE :keyword OR userip LIKE :keyword OR useragent LIKE :keyword OR timestamp LIKE :keyword ORDER BY id DESC LIMIT 1';
-	
-	/* Pagination Code starts 
-	$per_page_html = '';
-	$page = 1;
-	$start=0;
-	if(!empty($_POST["page"])) {
-		$page = $_POST["page"];
-		$start=($page-1) * ROW_PER_PAGE;
-	}
-	$limit=" limit " . $start . "," . ROW_PER_PAGE;
-	$pagination_statement = $pdo_conn->prepare($sql);
-	$pagination_statement->bindValue(':keyword', '%' . $search_keyword . '%', PDO::PARAM_STR);
-	$pagination_statement->execute();
-
-	$row_count = $pagination_statement->rowCount();
-	if(!empty($row_count)){
-		$per_page_html .= "<div style='text-align:center;margin:20px 0px;'>";
-		$page_count=ceil($row_count/ROW_PER_PAGE);
-		if($page_count>1) {
-			for($i=1;$i<=$page_count;$i++){
-				if($i==$page){
-					$per_page_html .= '<input type="submit" name="page" value="' . $i . '" class="btn-page current" />';
-				} else {
-					$per_page_html .= '<input type="submit" name="page" value="' . $i . '" class="btn-page" />';
-				}
-			}
-		}
-		$per_page_html .= "</div>";
-	}*/
-	
-	$query = $sql.$limit;
-	$pdo_statement = $pdo_conn->prepare($query);
-	$pdo_statement->bindValue(':keyword', '%' . $search_keyword . '%', PDO::PARAM_STR);
-	$pdo_statement->execute();
-	$result = $pdo_statement->fetchAll();
-?>
-    <?php
-		}
-	}
-	?>
-<?php echo $per_page_html; ?>
 </div>
 </div>
 </div>
