9f47c88c60f2074e37d5d79cf21528f2b1d5f255
textandmetal <textandmetal@gmail.com>
2020-01-07 10:47:15 +0000

    yes

--- analytics/articleviews.php
+++ analytics/articleviews.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "root", "GreenJeans33Winter1@", "buttons");
 
-$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = '/article-view.php'*");
+$result = mysqli_query($link, "SELECT * FROM analytics where pageurl = '/article-view.php'");
 $num_rows = mysqli_num_rows($result);
 
 echo "$num_rows Article Views\n";
