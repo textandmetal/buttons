8ad375144a1730f0a72e3b517b7e757739413ca3
textandmetal <textandmetal@gmail.com>
2020-01-07 08:48:55 +0000

    yes

--- analytics.php
+++ analytics.php
@@ -12,7 +12,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO pageurl (pageurl, urlquery, userip, useragent, timestamp)
+    $sql = "INSERT INTO analytics (pageurl, urlquery, userip, useragent, timestamp)
     VALUES ('$pageurl', '$urlquery', '$userip', '$useragent', '$date')";
     // use exec() because no results are returned
     $conn->exec($sql);
