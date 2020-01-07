1dc71b0906a6f2402c113810499a402438304daf
textandmetal <textandmetal@gmail.com>
2020-01-07 01:31:52 +0000

    yes

--- article-view.php
+++ article-view.php
@@ -67,11 +67,10 @@ foreach($results as $result)
 ?>
 <h1><a href="topic.php?id=<?php echo htmlentities($result->topic);?>"><?php echo htmlentities($result->field2);?></a></h1>
 <p><?php echo htmlentities($result->field3);?></p>
-<p>Author: <?php echo htmlentities($result->field4);?><br>Date: <?php echo htmlentities($result->field5);?></p>
 <p><?php echo htmlentities($result->field7);?></p>
-<form>
 
 <p>id: <?php echo htmlentities($result->id);?></p>
+<p>Author: <?php echo htmlentities($result->field4);?><br>Date: <?php echo htmlentities($result->field5);?></p>
 <?php }} ?>
 						</div>
 					</section>
