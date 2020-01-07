59454be8348663dbe4b43be242d310095dac1eff
textandmetal <textandmetal@gmail.com>
2020-01-07 08:58:18 +0000

    yes

---
+++ db-analytics.php
@@ -0,0 +1,5 @@
+<?php
+	$database_username = 'root';
+	$database_password = 'greenjeans33';
+	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=buttons', $database_username, $database_password );
+?>
